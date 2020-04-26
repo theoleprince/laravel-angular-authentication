3a3174979090df99ff0ad06ba0e6e065bbe741a0
theoleprince <jiozangtheophane@gmail.com>
2020-04-26 21:15:53 +0100

    Angular Routings

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { RequestResetComponent } from './password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './password/response-reset/response-reset.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
   { path: 'home', component: HomeComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'request-password-reset', component: RequestResetComponent},
+  { path: 'response-password-reset', component: ResponseResetComponent},
   { path: '404', component: NotfoundComponent},
   { path: '**', redirectTo:'404'}
 ];
